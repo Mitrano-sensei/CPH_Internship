5f4c8836bb4aba5b26579379c02d7fe5ee1ce086
Elyas TIGRE <elyastigre@gmail.com>
2024-06-13 13:50:17 +0200

    Locomotion 2

--- Assets/_Project/Scripts/HandAnimationController.cs
+++ Assets/_Project/Scripts/HandAnimationController.cs
@@ -13,30 +13,12 @@ public class HandAnimationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pinchAnimation.action.started += v =>
-        {
-            handAnimator.SetFloat("Pinch", v.ReadValue<float>());
-        };
-        pinchAnimation.action.performed += v =>
-        {
-            handAnimator.SetFloat("Pinch", v.ReadValue<float>());
-        };
-        pinchAnimation.action.canceled += v =>
-        {
-            handAnimator.SetFloat("Pinch", 0);
-        };
+        pinchAnimation.action.started += v =>   handAnimator.SetFloat("Pinch", v.ReadValue<float>());
+        pinchAnimation.action.performed += v => handAnimator.SetFloat("Pinch", v.ReadValue<float>());
+        pinchAnimation.action.canceled += v =>  handAnimator.SetFloat("Pinch", 0);
 
-        grabAnimation.action.started += v =>
-        {
-            handAnimator.SetFloat("Grab", v.ReadValue<float>());
-        };
-        grabAnimation.action.performed += v =>
-        {
-            handAnimator.SetFloat("Grab", v.ReadValue<float>());
-        };
-        grabAnimation.action.canceled += v =>
-        {
-            handAnimator.SetFloat("Grab", 0);
-        };
+        grabAnimation.action.started += v =>    handAnimator.SetFloat("Grab", v.ReadValue<float>());
+        grabAnimation.action.performed += v =>  handAnimator.SetFloat("Grab", v.ReadValue<float>());
+        grabAnimation.action.canceled += v =>   handAnimator.SetFloat("Grab", 0);
     }
 }
